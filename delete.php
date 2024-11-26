2409ce068df873ffce59a90b308fff96922750ae
heyitsgibsss <gilbertorlands28@gmail.com>
2024-11-26 13:17:25 +0800

    change all

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+require_once "config/database.php";
+
+$id = $_GET['id'];
+$sql = "DELETE FROM users WHERE id = $id";
+if ($conn->query($sql) === TRUE) {
+    header("Location: index.php");
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
